72da92cc57802b0070aed35cedf7201fbd32e600
pulkitkumar-01 <pulkitkumar0104@gmail.com>
2022-06-07 22:31:38 +0530

    My assignment on Mongo 2 session Book Model

---
+++ README.md
@@ -0,0 +1,3 @@
+
+Assignment
+https://docs.google.com/document/d/1afgPZSzSy1qgIP_iNJAVhZQD8eS-OJDxu8FpE0CzK5w/edit
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,19 +1,61 @@
 const express = require('express');
 const router = express.Router();
-// const UserModel= require("../models/userModel.js")
-const UserController= require("../controllers/userController")
-const BookController= require("../controllers/bookController")
+const bookModel = require('../models/bookModel')
+
 
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
 
-router.post("/createUser", UserController.createUser  )
+router.post("/createBook", async function (req, res) {     
+    let data = req.body
+    let saveData = await bookModel.create(data)
+    res.send({ msg : saveData })
+})
 
-router.get("/getUsersData", UserController.getUsersData)
+router.get("/booklist", async function (req, res) {      
 
-router.post("/createBook", BookController.createBook  )
+    let getlist = await bookModel.find().select( { bookName: 1, authorName:1, _id: 0} )
+    res.send({ msg : getlist })
+})
 
-router.get("/getBooksData", BookController.getBooksData)
+router.post("/getBooksInYear", async function (req, res) {     
+    let year = req.body.year
+    let getallBooks = await bookModel.find( {year:{ $eq: year}}).count()
+    
+    res.send({ msg : getallBooks })
 
-module.exports = router;
+})
+
+router.post("/getParticularBooks", async function (req, res) {      
+    let data = req.body
+    let getParticularBook = await bookModel.find(data)
+    
+    res.send({ msg : getParticularBook })
+
+})
+
+
+router.get("/getXINRBooks", async function (req, res) {
+    let returnBooks = await bookModel.find( { $or:
+        [ {"prices.indianPrice": { $eq: "200INR"}},
+          {"prices.indianPrice": { $eq: "500INR"}},
+          {"prices.indianPrice": { $eq: "100INR"}} 
+        ]})
+    res.send( { msg : returnBooks})
+
+})
+
+
+router.get("/getRandomBooks", async function (req, res) {
+        let returnStock = await bookModel.find( { $or:
+            [ {stockAvailable:true},
+              {totalPages: { $gt: 500}}
+            ]})
+            
+        res.send({ msg : returnStock })
+ })
+
+
+
+module.exports = router
